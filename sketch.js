d91979ba0adbb89861e21dc5c060aea5c4996085
Siddhartha Thakur <siddharthathakur@Siddharthas-MacBook-Air.local>
2020-10-04 20:41:27 +0530

    created constraint bodies

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint= Matter.Constraint
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintLog, chain;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +38,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    constraintLog= new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,constraintLog.body)
 }
 
 function draw(){
@@ -61,4 +65,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    constraintLog.display();
+    chain.display();
 }
